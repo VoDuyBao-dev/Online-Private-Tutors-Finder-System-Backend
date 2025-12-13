0b6e4346811a80216dbd080997c91613cb59cc25
ViLan026 <vil782991@gmail.com>
2025-12-13 12:35:26 +0700

    learner quản lí ebook và chỉnh thêm sử dụng patch

--- src/main/java/com/example/tutorsFinderSystem/configuration/SecurityConfig.java
+++ src/main/java/com/example/tutorsFinderSystem/configuration/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS", "PATCH"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
 
--- src/main/java/com/example/tutorsFinderSystem/repositories/EbookRepository.java
+++ src/main/java/com/example/tutorsFinderSystem/repositories/EbookRepository.java
@@ -3,9 +3,13 @@ package com.example.tutorsFinderSystem.repositories;
 import com.example.tutorsFinderSystem.entities.Ebook;
 import com.example.tutorsFinderSystem.enums.EbookType;
 
+import java.time.LocalDate;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 // import java.util.List;
 
@@ -15,7 +19,8 @@ public interface EbookRepository extends JpaRepository<Ebook, Long> {
 
     // List<Ebook> findByTitleContainingIgnoreCase(String keyword);
 
-    // List<Ebook> findByTypeAndTitleContainingIgnoreCase(EbookType type, String keyword);
+    // List<Ebook> findByTypeAndTitleContainingIgnoreCase(EbookType type, String
+    // keyword);
 
     Page<Ebook> findByType(EbookType type, Pageable pageable);
 
@@ -23,4 +28,17 @@ public interface EbookRepository extends JpaRepository<Ebook, Long> {
 
     Page<Ebook> findByTypeAndTitleContainingIgnoreCase(EbookType type, String keyword, Pageable pageable);
 
+    @Query("""
+                SELECT e FROM Ebook e
+                JOIN Tutor t ON e.uploadedBy.userId = t.user.userId
+                JOIN t.subjects s
+                WHERE (:subjectId IS NULL OR s.subjectId = :subjectId)
+                  AND (:type IS NULL OR e.type = :type)
+                  AND (:createdDate IS NULL OR DATE(e.createdAt) = :createdDate)
+            """)
+    Page<Ebook> searchEbooks(
+            @Param("subjectId") Long subjectId,
+            @Param("type") EbookType type,
+            @Param("createdDate") LocalDate createdDate,
+            Pageable pageable);
 }
